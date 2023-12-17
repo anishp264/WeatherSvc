9463122c388af6f89058331152a5d0f9a5d8332b
anishp264 <punnenanish@gmail.com>
2023-12-17 21:58:20 +0200

    Add project files.

---
+++ WeatherSvcTests/WeatherForecastControllerTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherSvcApi.Controllers;
+
+namespace WeatherSvcTests
+{
+    public class WeatherForecastControllerTest
+    {
+        [Fact]
+        public void WeatherForecastEndpoint_exists()
+        {
+            var controller = new WeatherForecastController();
+            var weatherForecasts = controller.Get();
+            if (weatherForecasts is not null)
+            {
+                Assert.True(weatherForecasts.Any());
+            }
+            //Assert.IsType<OkResult>(weatherForecasts);
+            Assert.False(false);
+        }
+    }
+}
